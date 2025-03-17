521c51cd04c49e0f02ebc900639b4d978d0f86f7
P130BTDG01 <juancarlos-098@hotmail.com>
2025-03-16 19:47:59 -0600

    Primer commit - subiendo mi proyecto

---
+++ js/Credenciales.js
@@ -0,0 +1,20 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    // Obtiene los valores ingresados en los campos de usuario y contraseña
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    
+    // Mostrará el mensaje de error
+    const errorMsg = document.getElementById('error-msg');
+
+    // Verifica si el usuario y la contraseña son correctos
+    if (username === "admin" && password === "1234") {
+        // Muestra un mensaje de éxito y redirige a otra página
+        alert("Login exitoso");
+        window.location.href = "Menu Ad.html"; // Redirige a otra página
+    } else {
+        // Si las credenciales son incorrectas muestra un mensaje de error
+        errorMsg.textContent = "Usuario o contraseña incorrectos";
+    }
+});
